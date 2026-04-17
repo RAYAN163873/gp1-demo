456f8d1ad89c8d2411e4a15843c51f3d0ed413ea
RAYAN163873 <rnalomari22@cit.just.edu.jo>
2026-04-18 02:15:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def add_numbers(a, b):
 
 @app.route("/")
 def home():
-    return jsonify({"status": "ok", "message": "Hello from gp1-demoo!"})
+    return jsonify({"status": "ok", "message": "Hello from gp1-demoo!Version 2.0 works!"})
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)  # nosec
