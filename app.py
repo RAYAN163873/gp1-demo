fbb2759892300f33509e5cede0598fc1b6740500
RAYAN163873 <rnalomari22@cit.just.edu.jo>
2026-04-18 01:08:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,5 @@ def add_numbers(a, b):
 def home():
     return jsonify({"status": "ok", "message": "Hello Dr. Tariq, Project 2 is Ready!,Hello from GitHub Desktop.!"})
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)  # nosec
+    app.run(host="127.0.0.1", port=8080)
