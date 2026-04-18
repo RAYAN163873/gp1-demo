b31eee56759782603d6bae1b79f4c7d7f6a2d644
RAYAN163873 <rnalomari22@cit.just.edu.jo>
2026-04-18 03:38:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def status():
     return jsonify({"status": "ok", "message": "Hello from gp1-demo!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)  # nosec
